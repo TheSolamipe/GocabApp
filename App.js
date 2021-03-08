4f4f15e0421399d7d4ee2b8e7e373b7f5943a949
Akinola Solamipe <akinolasolamipe@gmail.com>
2021-03-08 12:11:41 +0100

    Implemented Google places autocomplete feature

--- App.js
+++ App.js
@@ -8,14 +8,15 @@
 
 import React from 'react';
 import {StatusBar} from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+import DestinationSearch from './src/screens/DestinationSearch';
 
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <HomeScreen />
+      <DestinationSearch />
     </>
   );
 };
---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+import { View, TextInput, SafeAreaView } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import styles from './styles';
+import {GOOGLE_PLACES_KEY} from "@env";
+
+const DestinationSearch = () => {
+
+    const [originPlace, setOriginPlace] = useState(null);
+    const [destinationPlace, setDestinationPlace] = useState(null);
+
+    useEffect(()=>{
+        if(originPlace && destinationPlace){
+            console.warn('Redirect to result')
+        }
+    }, [originPlace, destinationPlace])
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <TextInput style={styles.textInput} value={fromText} onChangeText={setOriginPlace} placeholder='Current location' />
+                <GooglePlacesAutocomplete
+                    placeholder='Where to?'
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        setDestinationPlace( {data, details} );
+                        console.log(data, details);
+                    }}
+                    styles={{
+                        textInput: styles.textInput,
+                    }}
+                    fetchDetails
+                    query={{
+                        key: `${GOOGLE_PLACES_KEY}`,
+                        language: 'en',
+                    }}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default DestinationSearch
